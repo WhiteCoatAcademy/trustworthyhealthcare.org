619353732d6639fca240c5c243f9ca4b5293964e
Trustworthy Healthcare Initiative <team@trustworthyhealthcare.org>
2015-12-23 18:29:38 -0600

    Update to latest CMS api?

--- tools/raw-data/hospitals-by-state/0-update-json.sh
+++ tools/raw-data/hospitals-by-state/0-update-json.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-
-# This is the super neat Medicare API, via Socrata
-# It's open / public domain! :)
+# This script downloads .jsons of hospitals by state, via CMS.
 #
-# See: http://dev.socrata.com/foundry/#/data.medicare.gov/xubh-q36u
+# This is the super neat Medicare API, via Socrata: It's open / public domain! :)
+#
+# Latest: https://dev.socrata.com/foundry/#/data.medicare.gov/rbry-mqwu
+#
+# Stats:
+#  Total Rows: 4,805
+#  Source Domain: data.medicare.gov
+#  Created: 3/20/2015, 11:02:57 PM
+#  Last Updated: 12/9/2015, 6:36:05 PM
 
 states_and_territories=('AL' 'AK' 'AZ' 'AR' 'CA' 'CO' 'CT' 'DE' 'DC' 'FL' 'GA' 'HI' 'ID' 'IL' 'IN' 'IA' 'KS' 'KY' 'LA' 'ME' 'MD' 'MA' 'MI' 'MN' 'MS' 'MO' 'MT' 'NE' 'NV' 'NH' 'NJ' 'NM' 'NY' 'NC' 'ND' 'OH' 'OK' 'OR' 'PA' 'RI' 'SC' 'SD' 'TN' 'TX' 'UT' 'VT' 'VA' 'WA' 'WV' 'WI' 'WY' 'AS' 'GU' 'MP' 'PR' 'VI')
 
 
-# Add your API token here.
+## Add your API token here.
 # This works without, but might truncate / throttle stuff.
-# Yes, I know I'm committing this to a git repo. It's a free/public api.
+# (Yes, I know I'm committing this to a git repo. It's a free/public api.)
 app_token="aH1sk7hLaispfL5CPp1ezghkE"
+
 
 # https://soda.demo.socrata.com/resource/4tka-6guv?$$app_token=APP_TOKEN
 for state in "${states_and_territories[@]}"
@@ -20,6 +27,6 @@ do
     echo ${grabUrl}
     # NOTE: There are lots of awesome filters via the CMS data source.
     # We can filter by city, zip, hospital ownership, location region, etc.
-    curl "https://data.medicare.gov/resource/xubh-q36u.json?\$\$app_token=${app_token}&state=${state}" -o "${state}.json"
+    curl "https://data.medicare.gov/resource/rbry-mqwu.json?\$\$app_token=${app_token}&state=${state}" -o "${state}.json"
     sleep 3
 done
